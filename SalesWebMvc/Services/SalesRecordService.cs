14ff3c4701a72909613c78c76807ec09e46c1b31
Maria <mariaed.cassiano@gmail.com>
2023-02-21 15:11:21 -0300

    Implementando busca simples

---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SalesRecordService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            // Criando objeto Iqueryable para realizar consulta a partir do dbContext do SalesRecord
+            //A referência _context.SalesRecord é que aponta para a coleção correspondente aos dados do banco.
+            //Se quisermos acessar dados de SalesRecord, devemos começar por esta referência.
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
